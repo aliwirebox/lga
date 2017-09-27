5e35dcf27ef5e1488c95f4ed61826d5c587dea13
Arun Sahadeo <arun@wirebox.co.uk>
2017-09-27 16:02:35 +0000

    Update footer with company no.

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -29,6 +29,7 @@
             @foreach (config('brand.address') as $addressLine) 
                 <li>{{ $addressLine }}</li>
             @endforeach
+                <li>{{ config('brand.identity.companyno') }}</li>
             </ul>
             <p><a href="mailto:{{env('BRAND_SUPPORT_EMAIL')}}">{{env('BRAND_SUPPORT_EMAIL')}}</a></p>
             <p class="copy">&copy; All rights reserved {{config('brand.identity.legalname')}} {{ Carbon\Carbon::now()->format('Y') }}
