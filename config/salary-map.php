a904164667f200c0345d030f0efee24627fe67c2
Theo Kouzelis <theo@wirebox.co.uk>
2017-11-17 17:44:40 +0000

    Change salary options

--- config/salary-map.php
+++ config/salary-map.php
@@ -4,44 +4,36 @@ return [
     
     'candidate-options' => [
         0  => 'Any',
+        18 => '18k+',
+        22 => '22k+',
+        26 => '26k+',
         30 => '30k+',
-        35 => '35k+',
-        40 => '40k+',
-        45 => '45k+',
+        34 => '34k+',
+        38 => '38k+',
+        42 => '42k+',
+        46 => '46k+',
         50 => '50k+',
-        55 => '55k+',
-        60 => '60k+',
-        65 => '65k+',
-        70 => '70k+',
-        75 => '75k+',
-        80 => '80k+',
-        85 => '85k+',
-        90 => '90k+',
-        95 => '95k+',
-        100 => '100k+',
+        54 => '54k+',
+        58 => '58k+',
     ],
 
     'vacancy-options' => [
+        18 => 'to 18k',
+        22 => 'to 22k',
+        26 => 'to 26k',
         30 => 'to 30k',
-        35 => 'to 35k',
-        40 => 'to 40k',
-        45 => 'to 45k',
+        34 => 'to 34k',
+        38 => 'to 38k',
+        42 => 'to 42k',
+        46 => 'to 46k',
         50 => 'to 50k',
-        55 => 'to 55k',
-        60 => 'to 60k',
-        65 => 'to 65k',
-        70 => 'to 70k',
-        75 => 'to 75k',
-        80 => 'to 80k',
-        85 => 'to 85k',
-        90 => 'to 90k',
-        95 => 'to 95k',
-        100 => 'to 100k',
-        100000 => '100k+', 
+        54 => 'to 54k',
+        58 => 'to 58k',
+        100000 => '58k+',
         /*
-         * The 100k+ is effectively the "Any" option for hirers. To stop us putting a special 
-         * condition in the search for this option the number is set very high, much higher 
-         * than a candidate can ask for.  
+         * The 58k+ is effectively the "Any" option for hirers. To stop us putting a special
+         * condition in the search for this option the number is set very high, much higher
+         * than a candidate can ask for.
          */
     ],
 
