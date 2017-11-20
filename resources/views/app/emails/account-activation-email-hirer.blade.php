3eb8cdb4c327e5a189e7edf25e78ad9d67eab632
Theo Kouzelis <theo@wirebox.co.uk>
2017-11-20 17:23:20 +0000

    Fix hirer activation email

--- resources/views/app/emails/account-activation-email-hirer.blade.php
+++ resources/views/app/emails/account-activation-email-hirer.blade.php
@@ -3,7 +3,7 @@
 @section('greetingTerm', 'Dear')
 @section('content')
 
-    <p>Thank you for registering with {{  config('brand.identity.fullname')  }}. <a href="{{ url('email/verify/{$hirer->email_token}') }}">Click here to activate</a> your account and to be taken to your dashboard.</p>
+    <p>Thank you for registering with {{  config('brand.identity.fullname')  }}. <a href="{{ url("email/verify/{$hirer->email_token}") }}">Click here to activate</a> your account and to be taken to your dashboard.</p>
 
     <p>You can email us at <a href="mailto:{{  config('brand.email.support')  }}">{{  config('brand.email.support')  }}</a> or call us on {{ config('brand.phones.placeholder') }} if you have any questions.</p>
 
