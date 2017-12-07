167b96189e06984c89ca4a6b614e29ff7bf5caa3
Sohial Khan <sohial.khan@outlook.com>
2017-12-07 13:59:11 +0000

    Mobile menu/header change and Banner update

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -1,21 +1,18 @@
 <div class="navbar-header">
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar" aria-expanded="false">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span> 
-    </button>
+    
 </div>
 <!-- Top Header -->
 <header class="main-header">
     <div class="container">
-        <div class="col-sm-2 col-xs-5">
             <div class="logo">
-                <a href="{{url('/')}}"><img src="{{asset('img/logo.png')}}" class="img-responsive" /></a>
+                <a href="{{url('/')}}"><img src="{{asset('img/logo.png')}}" /></a>
             </div>
-        </div>
-        <div class="col-sm-8 col-xs-3 col-offset-sm-0 col-offset-xs-4">
-             <nav class="navbar">
-                 <div class="collapse navbar-collapse" id="myNavbar">
+            <a class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar" aria-expanded="false">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                 <nav class="navbar">
+                    <div class="collapse navbar-collapse" id="myNavbar">
                      <ul class="nav navbar-nav">
                         <li {{ (\Request::route() && \Request::route()->getName() == "home") ? 'class=active' : '' }}><a href="{{ route('home')}}">Home</a></li>
                         <li class="dropdown">
@@ -41,17 +38,15 @@
                         @else
                              <li><a class="cta red" href="{{url('register')}}">Sign Up</a></li>
                         @endif                     
-                                    
-                         
                      </ul>
                  </div>     
              </nav>
-        </div>
-        @if(!getGuard())
-            <a class="sign-in header-cta" href="{{url('login')}}"><i class="fa fa-lock"></i>  Sign In</a>
-        @else
-            <a class="logout header-cta" href="{{url('logout')}}"><i class="fa fa-sign-out"></i>  Logout</a>
-        @endif
+             @if(!getGuard())
+                <a class="sign-in header-cta" href="{{url('login')}}"><i class="fa fa-lock"></i>  Sign In</a>
+            @else
+                <a class="logout header-cta" href="{{url('logout')}}"><i class="fa fa-sign-out"></i>  Logout</a>
+            @endif
+      
     </div>
 </header>
 <!-- Start Top Nav -->
