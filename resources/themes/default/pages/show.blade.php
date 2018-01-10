def8ed73ec53efdf05e8d0d27bd7d65b53315d98
vagrant <vagrant@legal-asset-app>
2018-01-10 16:21:30 +0000

    Update banner title section

--- resources/themes/default/pages/show.blade.php
+++ resources/themes/default/pages/show.blade.php
@@ -3,22 +3,7 @@
 @section('title', $page->title)
 @section('seo_description', $page->seo_description)
 @section('seo_keywords', $page->seo_keywords)
-@section('bannerTitle')
-@php 
-    switch (true)
-    {
-        case stristr(strtolower($page->title), 'candidate faqs'):
-            echo '<span>Candidate FAQs</span>';
-            break;
-        case stristr(strtolower($page->title), 'about'):
-            echo '<span>About Us</span>';
-            break;
-        case stristr(strtolower($page->title), 'employers'):
-            echo '<span>Hirer FAQs</span>';
-            break;
-    }
-@endphp
-@endsection
+@section('bannerTitle', '<span>' . $page->title . '</span>')
 
 @section('content')
 
