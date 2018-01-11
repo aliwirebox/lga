36234d76f2ba2cd7a8bb29ea640ad862fd8dbfe1
vagrant <vagrant@legal-asset-app>
2018-01-11 12:06:52 +0000

    Blog disclaimer text

---
+++ resources/themes/default/partials/blog-disclaimer.blade.php
@@ -0,0 +1,11 @@
+    <section class="full-width-central">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2">
+                    <p class="red">
+                        @yield('disclaimer') 
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
