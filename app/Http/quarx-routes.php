ab3e19dfce0f6455d137630d16490d0d20384586
vagrant <vagrant@legal-asset-app>
2018-01-23 15:10:36 +0000

    Add blog route

--- app/Http/quarx-routes.php
+++ app/Http/quarx-routes.php
@@ -8,7 +8,10 @@
 
     Route::group(['namespace' => 'Quarx'], function () {
 
-        Route::get('blog', 'BlogController@all');
+        Route::get('blog', [
+            'as'   => 'frontend.blog',
+            'uses' => 'Quarx\BlogController@all',
+        ]);
         Route::get('blog/{url}', 'BlogController@show');
         Route::get('blog/tags/{tag}', 'BlogController@tag');
         Route::get('blog/categories/{category}', 'BlogCategoryController@show');
