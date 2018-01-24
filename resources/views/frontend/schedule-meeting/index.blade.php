478baffed9315cc716f9940f90d168c6ee0072e2
vagrant <vagrant@legal-asset-app>
2018-01-24 18:01:23 +0000

    #118: Make footer sticky below iframe

--- resources/views/frontend/schedule-meeting/index.blade.php
+++ resources/views/frontend/schedule-meeting/index.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <!-- Calendly inline widget begin -->
-<div class="calendly-inline-widget" data-url="https://calendly.com/legalasset" style="min-width:320px;height:580px;"></div>
+<div class="calendly-inline-widget" data-url="https://calendly.com/legalasset"></div>
 <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
 <!-- Calendly inline widget end -->
 @endsection
