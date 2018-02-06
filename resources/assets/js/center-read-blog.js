ccf9e0c704989d1da6cfdce3d29cce619fb8f5bc
Arun Sahadeo <arun@wirebox.co.uk>
2018-02-06 16:01:06 +0000

    Centering fixes for read blog

--- resources/assets/js/center-read-blog.js
+++ resources/assets/js/center-read-blog.js
@@ -6,16 +6,32 @@ function centerItem(elem, removeStyle)
 
     this.centeredItem = $(elem);
 
-    if ( removeStyle ) this.centeredItem.removeAttr('style');
+    if ( removeStyle )
+    {
+        this.centeredItem.removeAttr('style');
+        this.centeredItem.addClass('m-top-30');
+    };
 
     if ( !$(this.centeredItem).length || ( $(window).width() < 1200 ) ) return;
 
     this.init = function()
     {
         var containerHeight = this.centeredItem.parents(".row").css("height");
-        $(this.centeredItem).css({
-            marginTop: "calc(" + containerHeight + " / 3)"
-        });
+        if ( $(this.centeredItem).hasClass('m-top-30') ) $(this.centeredItem).removeClass('m-top-30');
+        if ( $(window).width() >= 1600 )
+        {
+            $(this.centeredItem).css({
+                marginTop: "calc(" + containerHeight + " / 2)",
+                right: "35%",
+                width: "400px"
+            });
+        }
+        else 
+        {
+            $(this.centeredItem).css({
+                marginTop: "calc(" + containerHeight + " / 3)",
+            });
+        }
     };
 
     this.init();
