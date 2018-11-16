bfe6ab273c1e6250aaaa6aca9c7970145ab07548
Theo Kouzelis <theo@wirebox.co.uk>
2018-11-16 13:12:19 +0000

    Add salary options

--- config/salary-map.php
+++ config/salary-map.php
@@ -15,6 +15,16 @@ return [
         50 => '50k+',
         54 => '54k+',
         58 => '58k+',
+        62 => '62k+',
+        66 => '66k+',
+        70 => '70k+',
+        74 => '74k+',
+        78 => '78k+',
+        82 => '82k+',
+        86 => '86k+',
+        90 => '90k+',
+        94 => '94k+',
+        98 => '98k+',
     ],
 
     'vacancy-options' => [
@@ -29,9 +39,18 @@ return [
         50 => 'to 50k',
         54 => 'to 54k',
         58 => 'to 58k',
-        100000 => '58k+',
+        62 => 'to 62k',
+        66 => 'to 66k',
+        70 => 'to 70k',
+        74 => 'to 74k',
+        78 => 'to 78k',
+        82 => 'to 82k',
+        86 => 'to 86k',
+        90 => 'to 90k',
+        94 => 'to 94k',
+        100000 => '98k+',
         /*
-         * The 58k+ is effectively the "Any" option for hirers. To stop us putting a special
+         * The 98k+ is effectively the "Any" option for hirers. To stop us putting a special
          * condition in the search for this option the number is set very high, much higher
          * than a candidate can ask for.
          */
