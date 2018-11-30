ba9c95071026e90f36c22aa9b2ed780f34d5a4ae
Your Name <kamilahagbaje@gmail.com>
2018-11-30 11:18:09 +0000

    compiling css

--- resources/views/frontend/contact-us/index.blade.php
+++ resources/views/frontend/contact-us/index.blade.php
@@ -75,6 +75,10 @@
                     <div class="row">
                         <div class="col-sm-4 col-sm-offset-4">
                             <div class="form-group">
+                                <div class="g-recaptcha" data-sitekey="6Les330UAAAAAAK0VImrurQl-tMnRSwqzKqBCI0S"></div>
+
+
+                                <br>
                                 <button class="btn btn-primary btn-lg btn-block">Submit</button>
                             </div>
                         </div>
