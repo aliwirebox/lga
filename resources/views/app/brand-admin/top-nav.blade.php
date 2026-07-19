57a3e9d0a1796934f3f76a9e3febc9b8e84c155f
Ali Rasheed <arsalirasheed@gmail.com>
2026-07-19 20:50:19 +0100

    Updated

--- resources/views/app/brand-admin/top-nav.blade.php
+++ resources/views/app/brand-admin/top-nav.blade.php
@@ -11,7 +11,6 @@
         <li><a href="#"
        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
         <i class="fa fa-sign-out"></i>
-        Logout
     </a>
 
     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
